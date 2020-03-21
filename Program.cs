864072a66c62048814231726dbbffc3296c7af6d
naufal <naufalhaxor78@gmail.com>
2020-03-21 14:39:42 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            // pengesetan nilai properties
+            taxi.DriverName = "Naufal";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // pemanggilan method
+            taxi.TaxiInfo();
+
+            if (taxi.OnDuty == true)
+            {
+                // Taxi sedang mengantar penumpang
+                taxi.PickUpPassenger();
+                taxi.DropOffPassenger();
+            }
+            else if(taxi.OnDuty == false)
+            {
+                // Taxi sedang tidak mengantar penumpang
+                Console.WriteLine("Taxi tersedia");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApp
+{
+    class Taxi
+    {
+        public string DriverName;
+        public bool OnDuty;
+        public int NumPassenger;
+        string status;
+
+        // fungsi untuk menampilkan info taxi
+        public void TaxiInfo()
+        {
+            // menampilkan nama driver
+            Console.WriteLine("Driver Name : " + DriverName); 
+
+            // fungsi jika OnDuty true maka status berubah menjadi yes atau sebaliknya
+            if (OnDuty == true)
+            {
+                status = "Yes";
+            }
+            else
+            {
+                status = "No";
+            }
+
+            // fungsi jika OnDuty true maka jumlah penumpang akan menjadi 10 atau jika false maka jumlah penumpang menjadi 0
+            if (OnDuty == true)
+            {
+                NumPassenger = 10;
+            }
+            else
+            {
+                NumPassenger = 0;
+            }
+
+            // menampilkan status driver
+            Console.WriteLine("On Duty : " + status);
+
+            // menampilkan jumlah penumpang
+            Console.WriteLine("Number of Passenger : " + NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine(DriverName + " sedang menjemput penumpang");
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine(DriverName + " sedang mengantar penumpang");
+        }
+    }
+
+}
